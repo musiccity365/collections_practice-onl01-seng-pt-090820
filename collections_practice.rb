c189cd39f7da2e078537ebecb4919606f2764a33
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-19 02:24:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
+
+def find_a(array)
+  array.select {|word| word.chr == "a" }
+end
+
+def sum_array(array)
+  array.inject {|sum, num| sum + num}
+end
+
+def add_s(array)
+  array.collect.with_index {|word, index|
+    index == 1 ? word : word + "s"
+  }
+end
